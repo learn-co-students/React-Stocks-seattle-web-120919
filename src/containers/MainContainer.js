01512d6982e691dd4d3318ae0f8e142db4d9ee50
Azhang9328 <azhang9328@gmail.com>
2020-02-25 12:16:17 -0800

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,90 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocksList: [],
+    ownedStocks: [],
+    allStocks: []
+  }
+
+  componentDidMount(){
+    this.fetchStocks()
+  }
+
+  fetchStocks = () => {
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(data => this.setState({
+        allStocks: data,
+        stocksList: data
+      })
+    )
+  }
+
+  moveStock = (passedStock) => {
+    if(!this.state.ownedStocks.includes(passedStock)){
+      this.setState({
+        ...this.state,
+        ownedStocks: [
+          ...this.state.ownedStocks,
+          passedStock
+        ]
+      })
+    } else {
+      this.setState({
+        ...this.state,
+        ownedStocks: this.state.ownedStocks.filter(stock => {
+          return stock.id !== passedStock.id
+        })
+      })
+    }
+  }
+
+  sortByTicker = () => {
+    this.setState({
+      ...this.state,
+      stocksList: this.state.stocksList.sort((a, b) => {
+        let nA = a.ticker.toUpperCase()
+        let nB = b.ticker.toUpperCase()
+        if(nA < nB) {return -1}
+        if(nA > nB) {return 1}
+        return 0
+      })
+    })
+  }
+
+  sortByPrice = () => {
+    this.setState({
+      ...this.state,
+      stocksList: this.state.stocksList.sort((a,b) => {
+        return a.price - b.price
+      })
+    })
+  }
+
+  filterStocks = (type) => {
+    this.setState({
+      ...this.state,
+      stocksList: this.state.allStocks.filter(stock => {
+        return stock.type === type
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortByTicker={this.sortByTicker} sortByPrice={this.sortByPrice} filterStocks={this.filterStocks}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocksList={this.state.stocksList} moveStock={this.moveStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer ownedStocks={this.state.ownedStocks} moveStock={this.moveStock}/>
 
             </div>
           </div>
